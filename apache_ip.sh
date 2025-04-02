d73be6c420cb94657313fe82a271000de7fcc080
dharmveer0876 <53742778+dharmveer0876@users.noreply.github.com>
2025-04-02 20:50:22 +0530

    Create apache_ip.sh

---
+++ apache_ip.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#The Apache access logs are text files that include information about all the requests processed by the Apache server
+
+# Printing top 10 IPs hits counts
+
+echo "Printing top 10 IPs hits counts: "
+
+cat apache_logs | awk '{ print $1 }' | sort | uniq -c | sort -r | head -10
+
+#Printing status code for http requests
+
+echo "Printing status code for http requests:"
+
+grep -i "HTTP" apache_logs | awk '{ print $9 }' | sort | uniq -c | sort -r
+
+echo "printing the bots used in the apache log:"
+
+grep -i "HTTP" apache_logs | grep "bot" | awk '{ print $12 }' | sort | uniq | sort -r | sed -e 's/^"//'
